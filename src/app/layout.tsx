2e97ccf3e66b2436770fd168a06493129cd59ad0
x-sss-x <jbportalsblr@gmail.com>
2025-03-03 16:00:16 +0530

    Update font configuration to include subsets for Roboto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Roboto } from "next/font/google";
 
 const RobotoFont = Roboto({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  style: ["normal", "italic"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
